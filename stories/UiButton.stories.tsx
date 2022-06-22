d7250336dda522d45a4a45f5d11fbde7115ba697
Tomasz <tomasz93szarek@gmail.com>
2022-06-22 23:37:11 +0200

    Add styling

--- stories/UiButton.stories.tsx
+++ stories/UiButton.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 
-import { UiButton, ButtonTypes } from "../src/components/UiButton";
+import { UiButton, ButtonTypes } from "../dist/components/UiButton";
 
 export default {
   title: 'UiButton',
